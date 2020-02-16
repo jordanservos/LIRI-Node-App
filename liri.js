75a876a873c2836ed4f6cc1f7d9c2923dd55e63f
Jordan Servos <jservos11@yahoo.com>
2020-02-16 12:15:43 -0700

    added keys and variables

--- liri.js
+++ liri.js
@@ -1,5 +1,8 @@
-require("dotenv").config();
-
-var keys = require("./keys.js");
+const dotenv = require("dotenv").config();
+var Spotify = require('node-spotify-api');
+const keys = require("./keys.js");
+var request = require('request');
+var fs = require('fs');
 
 var spotify = new Spotify(keys.spotify);
+var omdbKey = keys.omdb.api_key;
